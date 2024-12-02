a7cedef72393a700f47d8086e9fe509448955d7c
kxelina <elina88.k@gmail.com>
2024-12-02 15:56:28 +0200

    osa12:12.12

--- todo-app/todo-backend/routes/index.js
+++ todo-app/todo-backend/routes/index.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const router = express.Router();
+const redis = require('../redis');
 
 const configs = require('../util/config')
 
@@ -14,5 +15,13 @@ router.get('/', async (req, res) => {
     visits
   });
 });
+
+router.get('/statistics', async (req, res) => {
+  const newTodos = await redis.getAsync('added_todos')
+
+  res.send({
+    added_todos: parseInt(newTodos) || 0
+  })
+})
 
 module.exports = router;
